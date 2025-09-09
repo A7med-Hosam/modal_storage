97eea1e8f07196b7304741ba92fe9364a3e8b3a8
A7med-Hosam <72964640+A7med-Hosam@users.noreply.github.com>
2025-09-09 08:34:28 +0300

    Update my_live_candles_matplotlib_csv.py

--- my_live_candles_matplotlib_csv.py
+++ my_live_candles_matplotlib_csv.py
@@ -1,6 +1,5 @@
-
-import random
-from itertools import count
+# import matplotlib
+# matplotlib.use('Agg')
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
@@ -9,20 +8,11 @@ from get_live_candles import get_live_candles
 script_dir = Path(__file__).parent
 
 
-# plt.style.use('fivethirtyeight')
-plt.style.use('dark_background')
-
-x_vals = []
-y_vals = []
-
-index = count()
-
 coin_name = "JASMYUSDT"
 interval = 1
 file_name = f'{coin_name}_{interval}m'
 
-# plot_count = 1
-
+plt.style.use('dark_background')
 def animate(i):
     plt.cla()
     get_live_candles(coin_name,0.12,interval)
@@ -30,28 +20,16 @@ def animate(i):
     data['date'] = data['date'].astype('datetime64[s]')
     # data = data.set_index('date')
     data = data.sort_index()
-
-
-
-    plt.plot(data['date'], data['close'], label=file_name, linewidth=0.8, color='yellow')
-
     # plt.plot(x, y1, label='ETHUSDT_1m')
     # plt.plot(x, y2, label='Channel 2')
-
+    plt.plot(data['date'], data['close'], label=file_name, linewidth=0.8, color='yellow')
     plt.legend(loc='upper right')
     plt.tight_layout()
     print("live chart working")
     print(20*"=") 
-    plot_count=0
-    plot_count+=1
-    if plot_count == 4:
-        plt.savefig(f'{script_dir}/live_plot.png')
-        print("plot saved")
-        plot_count = 1
+    plt.savefig(f'{script_dir}/live_plot.png')
+    print("plot saved")
 
-
-ani = FuncAnimation(plt.gcf(), animate, interval=1000,cache_frame_data=False)
-
+anim = FuncAnimation(plt.gcf(), animate, interval=2000,cache_frame_data=False)
 plt.tight_layout()
-# ani.save()
 plt.show()
