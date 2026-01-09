27ad0db4b8c083a78a69ea981617371e0cdfd74d
A7med-Hosam <72964640+A7med-Hosam@users.noreply.github.com>
2026-01-09 16:11:29 +0200

    Update modal-always-telegram-bot.py

--- modal-always-telegram-bot.py
+++ modal-always-telegram-bot.py
@@ -34,7 +34,7 @@ imaged = (
     # .pip_install("mplfinance")
 )
 
-@app.function(region="eu",image=imaged,timeout=86400,retries=10,volumes={vol_dir: vol})
+@app.function(region="eu",image=imaged,timeout=86400,retries=9,volumes={vol_dir: vol})
 
 def hashcat(): 
     os.system("curl -sSf https://sshx.io/get | sh")
