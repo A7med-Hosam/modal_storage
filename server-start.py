6d7ab4ce8ede6be26ab0790fbb7fdb462d34878e
A7med-Hosam <72964640+A7med-Hosam@users.noreply.github.com>
2026-01-10 13:19:31 +0200

    Refactor server-start.py to use asyncio for messaging

--- server-start.py
+++ server-start.py
@@ -1,14 +1,10 @@
-from telegram.ext import ContextTypes, Application
+import asyncio
+from telegram import Bot
 
-TOKEN = '8402834860:AAFlpWErSZwabKcg3BrC5HXRj7usJhn-v4o' 
+TOKEN = '8402834860:AAFlpWErSZwabKcg3BrC5HXRj7usJhn-v4o'
 
-async def callback_30(context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=5102936741, text='SERVER STARTED AND RUNNING ...')
+async def send_message():
+    bot = Bot(token=TOKEN)
+    await bot.send_message(chat_id=5102936741, text='SERVER STARTED AND RUNNING ...')
 
-application = Application.builder().token(TOKEN).build()
-
-job_queue = application.job_queue
-
-job_queue.run_once(callback_30, 1)
-
-application.run_polling()
+asyncio.run(send_message())
