d7f51af1e2f87a9421fe809f494bf4896eef8022
A7med-Hosam <72964640+A7med-Hosam@users.noreply.github.com>
2026-02-25 18:25:56 +0200

    Enable message sending via command-line arguments
    Added command-line argument support for sending messages.

--- telegram_message.py
+++ telegram_message.py
@@ -1,4 +1,5 @@
 import asyncio
+import sys
 from telegram import Bot
 
 TOKEN = '8402834860:AAFlpWErSZwabKcg3BrC5HXRj7usJhn-v4o'
@@ -7,4 +8,9 @@ async def send_message(message):
     bot = Bot(token=TOKEN)
     await bot.send_message(chat_id=5102936741, text=message)
 
-# asyncio.run(send_message('Server is exiting - restarting now ....'))
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        message_to_send = " ".join(sys.argv[1:])
+        asyncio.run(send_message(message_to_send))
+    else:
+        print("Usage: python3 telegram_message.py 'Your message here'")
